c26ff7228b6079fcdacc957ec4724d60cabe3ffb
Nazli64 <80019852+Nazli64@users.noreply.github.com>
2021-03-17 11:37:45 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+//  Refer to  html canvas element
+const canvas = document.getElementById("canva");
+ 
+// Drawing using 2D interface
+const ctx = canvas.getContext('2d');
+ 
+// Set Canvas size
+canvas.height = 500;
+canvas.width = 500;
+
+//Find center coordinate of your canvas
+//YOUR CODE HERE
+
+
+//Draw a circle, Center of the circle is at the center of the canvas.
+// Radius of the circle is 200px
+//
+//YOUR CODE HERE
